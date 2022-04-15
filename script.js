01f72e3bf71a7ed1b1d64f37057aea73781be025
Noor Ali <nooralib180@gmail.com>
2022-04-15 22:29:07 +0500

    updated

--- script.js
+++ script.js
@@ -112,10 +112,16 @@ const account1 = {
 };
 
 // NOTE: below two lines of codes are for default settings...
-// const accounts = [account1];
-// localStorage.setItem("accounts",JSON.stringify(accounts));
+let accounts;
+if(!localStorage.getItem('accounts')){
+    accounts = [account1];
+    localStorage.setItem("accounts",JSON.stringify(accounts));
+}
+else{
+    accounts = JSON.parse(localStorage.getItem('accounts'));
+}
 
-const accounts = JSON.parse(localStorage.getItem('accounts'));
+
 let currentAccount, currentAccountIndex, timer;
 
 // FUNCTIONALITIES...
